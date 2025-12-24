9878522d17446a817ad1f9da63385f9169742b33
Shubham Hire <shubham.hire2006@gmail.com>
2025-12-24 22:52:04 +0530

    Fix PostgreSQL datetime compatibility

--- tourguard-backend Final/src/users/entities/user.entity.ts
+++ tourguard-backend Final/src/users/entities/user.entity.ts
@@ -31,7 +31,7 @@ export class User {
   @Column({ default: false })
   otpVerified: boolean;
 
-  @Column({ type: 'datetime', nullable: true })
+  @Column({ type: 'timestamp', nullable: true })
   lastLogin?: Date;
 
   @CreateDateColumn()
